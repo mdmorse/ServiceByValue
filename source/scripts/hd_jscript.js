b543f0df388c3cbb882e56dcec70f78cdf765d40
Matt Morse <mmorse@comcast.net>
2013-10-03 13:41:37 -0600

    latest

--- source/scripts/hd_jscript.js
+++ source/scripts/hd_jscript.js
@@ -1,84 +1,49 @@
 function clickButton(locale) {
     window.location = locale;
-}
-var hideOthers = new Array();
-var classIdentifier = "class";
-if (navigator.appName == 'Microsoft Internet Explorer') {
+}var hideOthers = new Array();var classIdentifier = "class";if (navigator.appName == 'Microsoft Internet Explorer') {
     classIdentifier = "className";
-}
-
-function getElementsByName_iefix(tag, name) {
-    var elem = document.getElementsByTagName(tag);
-    var arr = new Array();
-    for (i = 0, iarr = 0; i < elem.length; i++) {
-        att = elem[i].getAttribute(classIdentifier);
-        if (att == name) {
-            arr[iarr] = elem[i];
-            iarr++;
+}function getElementsByName_iefix(tag, name) {
+    var elem = document.getElementsByTagName(tag);    var arr = new Array();    for (i = 0, iarr = 0; i < elem.length; i++) {
+        att = elem[i].getAttribute(classIdentifier);        if (att == name) {
+            arr[iarr] = elem[i];            iarr++;
         }
-    }
-    return arr;
-}
-
-function showElem(elemId) {
+    }    return arr;
+}function showElem(elemId) {
     for (var i = 0; i < hideOthers.length; i++) {
-        var len2 = getElementsByName_iefix("DIV", hideOthers[i]).length;
-        for (var t = 0; t < len2; t++) {
+        var len2 = getElementsByName_iefix("DIV", hideOthers[i]).length;        for (var t = 0; t < len2; t++) {
             getElementsByName_iefix("DIV", hideOthers[i])[t].style.visibility = "hidden";
         }
-    }
-    var len = getElementsByName_iefix("DIV", elemId).length;
-    for (var i = 0; i < len; i++) {
+    }    var len = getElementsByName_iefix("DIV", elemId).length;    for (var i = 0; i < len; i++) {
         getElementsByName_iefix("DIV", elemId)[i].style.visibility = "visible";
     }
-}
-
-function hideElem(elemId) {
-    var len = getElementsByName_iefix("DIV", elemId).length;
-    for (var i = 0; i < len; i++) {
+}function hideElem(elemId) {
+    var len = getElementsByName_iefix("DIV", elemId).length;    for (var i = 0; i < len; i++) {
         getElementsByName_iefix("DIV", elemId)[i].style.visibility = "hidden";
     }
-}
-
-function is_child_of(parent, child) {
+}function is_child_of(parent, child) {
     if (child != null) {
         while (child.parentNode) {
             if ((child = child.parentNode) == parent) {
                 return true;
             }
         }
-    }
-    return false;
-}
-
-function is_sibling_of(parent, child) {
+    }    return false;
+}function is_sibling_of(parent, child) {
     if (child != null) {
-        var elemId = parent.getAttribute(classIdentifier);
-        var childTag = child.tagName;
-        while (childTag != "DIV") {
-            child = child.parentNode;
-            childTag = child.tagName;
-            if (child == window) {
+        var elemId = parent.getAttribute(classIdentifier);        var childTag = child.tagName;        while (childTag != "DIV") {
+            child = child.parentNode;            childTag = child.tagName;            if (child == window) {
                 return false;
             }
-        }
-        var childId = child.getAttribute(classIdentifier);
-        if (childId == elemId) {
+        }        var childId = child.getAttribute(classIdentifier);        if (childId == elemId) {
             return true;
         }
-    }
-    return false;
-}
-
-function fixOnMouseOut(element, event, jscp) {
-    var current_mouse_target = null;
-    if (event.toElement) {
+    }    return false;
+}function fixOnMouseOut(element, event, jscp) {
+    var current_mouse_target = null;    if (event.toElement) {
         current_mouse_target = event.toElement;
-    }
-    else if (event.relatedTarget) {
+    }    else if (event.relatedTarget) {
         current_mouse_target = event.relatedTarget;
-    }
-    if (!is_child_of(element, current_mouse_target) && !is_sibling_of(element, current_mouse_target) && element != current_mouse_target) {
+    }    if (!is_child_of(element, current_mouse_target) && !is_sibling_of(element, current_mouse_target) && element != current_mouse_target) {
         eval(jscp);
     }
 }
